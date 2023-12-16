603baf31c7eeab193544bad46d7e57a32f103d15
lvxnull <86745229+lvxnull@users.noreply.github.com>
2023-12-16 18:22:49 +0100

    Use the latest stable quilt

--- src/main/java/org/lvxnull/allowlist/AllowListStorage.java
+++ src/main/java/org/lvxnull/allowlist/AllowListStorage.java
@@ -1,7 +1,5 @@
 package org.lvxnull.allowlist;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
